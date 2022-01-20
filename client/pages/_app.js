6b11fbc7a042e80e727bb27484815a95a39c08aa
Romaric Zounlome <rzounlom@gmail.com>
2022-01-20 11:02:11 -0500

    inital project setup

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "antd/dist/antd.css";
+import "../public/css/styles.global.scss";
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
---
+++ client/pages/index.js
@@ -0,0 +1,13 @@
+const Index = () => {
+  return (
+    <>
+      <h1 className="jumbotron d-flex justify-content-center align-items-center text-white">
+        Hello world
+      </h1>
+
+      <p>From NextJs</p>
+    </>
+  );
+};
+
+export default Index;
